008aecc6ab6d42a825d027a8404eac8713945754
Oliver <oliverkrue@googlemail.com>
2020-07-18 01:31:25 +0200

    cleanup template and exchanged mysql connector to nanodbc

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,13 @@
 #include <intercept.hpp>
-
-#include "ReplayPart.h"
-
-#include <nlohmann/json.hpp>
-
-#include <cpr/cpr.h>
-
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/ini_parser.hpp>
-
 #include <string>
-#include <iostream>
+#include <windows.h>
+#include <string> 
 #include <sstream>
-#include <fstream>
-#include <iomanip>
-#include <ctime>
-#include <chrono>
-#include <filesystem>
-#include <memory>
-#include <algorithm>
+#include <mysqlx/xdevapi.h>
 
-using namespace intercept;
+namespace intercept::types {
+	extern bool exiting;
+};
 
 int intercept::api_version() {
     return INTERCEPT_SDK_API_VERSION;
@@ -29,14 +16,36 @@ int intercept::api_version() {
 void intercept::register_interfaces() {}
 
 void intercept::pre_init() {
-
+	intercept::sqf::system_chat("Test is running");
 }
 
 void intercept::pre_start() {
+    //intercept::sqf::create_vehicle("blub", *pos);
+}
 
+void display_fps() {
+	while (!exiting)
+	{
+		{
+			intercept::client::invoker_lock thread_lock;
+			auto fps = std::to_string(intercept::sqf::diag_fps());
+			const auto pos = dynamic_cast<game_data_object*>(intercept::sqf::player().data.getRef())->get_position_matrix()._position;
+			std::ostringstream stream;
+			stream << "POS: " << pos.x << "|" << pos.y << "|" << pos.z;
+			intercept::sqf::hint(stream.str());
+			intercept::sqf::system_chat(fps);
+		}
+		Sleep(500);
+	}
 }
 
 void intercept::post_init() {
-   
+	std::thread display_thread(display_fps);
+	display_thread.detach();
+	
 }
 
+void intercept::mission_ended()
+{
+	
+}
